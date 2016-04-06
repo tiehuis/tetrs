2080140bdd25df2b47c37002892e6ed074f1737f
= <marctiehuis@gmail.com>
2016-04-06 06:20:00 +0000

    [rebuild-pages] 3a12b1d

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tetrs'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='tetrs/field/struct.Field.html' title='tetrs::field::Field'>Field</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='tetrs/controller/struct.Controller.html' title='tetrs::controller::Controller'>Controller</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='tetrs/rotation/srs/struct.SRS.html' title='tetrs::rotation::srs::SRS'>SRS</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
