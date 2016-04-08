abc75d180029334e658f24ee2e4c5d8f3196edb5
= <marctiehuis@gmail.com>
2016-04-08 05:45:13 +0000

    [rebuild-pages] 8ce0dfd

--- tetrs/wallkick/sidebar-items.js
+++ tetrs/wallkick/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["SRS","Wallkick"]],"trait":[["Wallkick","Trait which specifies what wallkick tests must implement. Every wallkick test must implement an iterator with offsets of type (i32, i32)."]]});
+initSidebarItems({"struct":[["DTET","Wallkick"],["Empty","Wallkick"],["SRS","Wallkick"],["Simple","Wallkick"]],"trait":[["Wallkick","Trait which specifies what wallkick tests must implement. Every wallkick test must implement an iterator with offsets of type (i32, i32)."]]});
