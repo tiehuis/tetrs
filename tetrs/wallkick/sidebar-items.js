1a37bd73227266b219dcbc9b17e20c8ced5fc502
= <marctiehuis@gmail.com>
2016-04-15 10:27:46 +0000

    [rebuild-pages] f958b88

--- tetrs/wallkick/sidebar-items.js
+++ tetrs/wallkick/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["new","Factory function for constructing rotation systems from name."]],"struct":[["DTET",""],["Empty",""],["SRS",""],["Simple",""],["TGM",""]],"trait":[["Wallkick","The `Wallkick` trait must be implemented by all wallkicks."]]});
+initSidebarItems({"fn":[["new","Factory function for constructing rotation systems from name."]],"struct":[["DTET",""],["Empty",""],["SRS",""],["Simple",""],["TGM",""],["TGM3",""]],"trait":[["Wallkick","The `Wallkick` trait must be implemented by all wallkicks."]]});
