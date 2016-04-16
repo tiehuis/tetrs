62180cf03979e4cd03dde4cafb5d59da6c4016e6
= <marctiehuis@gmail.com>
2016-04-16 11:53:43 +0000

    [rebuild-pages] 87eeb56

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tetrs'] = ["impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='tetrs/field/struct.FieldOptions.html' title='tetrs::field::FieldOptions'>FieldOptions</a>","impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='tetrs/engine/struct.EngineOptions.html' title='tetrs::engine::EngineOptions'>EngineOptions</a>","impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='tetrs/engine/struct.EngineSettings.html' title='tetrs::engine::EngineSettings'>EngineSettings</a>",];
+implementors['tetrs'] = ["impl <a class='trait' href='https://serde-rs.github.io/serde/serde/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='struct' href='tetrs/field/struct.FieldOptions.html' title='tetrs::field::FieldOptions'>FieldOptions</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
